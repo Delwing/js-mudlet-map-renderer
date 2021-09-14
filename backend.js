6cc720f0c2b2a9411374b487db953d9d314de5f7
Piotr Wilczynski <delwing@gmail.com>
2021-09-14 22:12:14 +0200

    minor changes

--- backend.js
+++ backend.js
@@ -9,7 +9,7 @@ let data = mapData;
 
 let reader = new MapReader(data, colors);
 
-let idLimits = 4723;
+let idLimits = 6295;
 
 let roomLimits = reader.roomIndex[idLimits];
 let area = reader.getAreaById(roomLimits.id);
@@ -19,6 +19,7 @@ let limit = false;
 
 let renderer = new Renderer(null, reader, area, reader.getColors(), {scale: scale, roomSize: 1, gridSize: 2, renderLabels: true});
 renderer.renderSelection(idLimits);
+renderer.renderPosition(idLimits)
 renderer.render();
 let bounds
 if (!limit) {
