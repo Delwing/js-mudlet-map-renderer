f1052504151f1e5591b4122c0d4a912e98498d2c
Piotr Wilczyński <delwing@gmail.com>
2025-07-18 14:08:45 +0200

    fix rendering

--- map-fragment/draw/renderer.js
+++ map-fragment/draw/renderer.js
@@ -67,7 +67,6 @@ class Renderer {
         this.innerExits = ["up", "down", "u", "d", "in", "out", "i", "u"];
         this.paper = new paper.PaperScope();
         this.targetElement = element;
-        this.bounds = this.area.getAreaBounds(this.settings.uniformLevelSize);
         if (this.targetElement === undefined) {
             this.element = new paper.Size((this.bounds.width + padding * 2) * this.scale, (this.bounds.height + padding * 2) * this.scale);
             this.isVisual = false;
